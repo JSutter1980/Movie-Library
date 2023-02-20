012bea7c84efdf6540312403209b9ed4dc660c0d
Joe Sutter <jsutter6@my.wctc.edu>
2023-02-20 13:24:53 -0600

    {1.02} add menu and functions

--- Program.cs
+++ Program.cs
@@ -1,18 +1,8 @@
 ﻿using NLog;
-
-// See https://aka.ms/new-console-template for more information
 string path = Directory.GetCurrentDirectory() + "\\nlog.config";
-
 // create instance of Logger
 var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
 
-// log sample messages
-logger.Trace("Sample trace message");
-logger.Debug("Sample debug message");
-logger.Info("Sample informational message");
-logger.Warn("Sample warning message");
-logger.Error("Sample error message");
-logger.Fatal("Sample fatal error message\n");
 
 Console.WriteLine("Welcome to Blockbuster Video Database!");
 Console.WriteLine("What would you like to do today?");
@@ -23,21 +13,33 @@ string? resp = Console.ReadLine();
 
 if (resp == "1")
 {
-    if (File.Exists("movies.csv"))
-    {
-        StreamReader sr = new StreamReader("movies.csv");
-        while (!sr.EndOfStream)
-        {
-            string line = sr.ReadLine();
-            string[] arr = line.Split(',');
+            if (File.Exists("movies.csv"))
+            {
+                StreamReader sr = new StreamReader("movies.csv");
+                while (!sr.EndOfStream)
+                {
 
-            Console.WriteLine("{0}, {1}", arr);
-            
+                    string line = sr.ReadLine();
+                    string[] arr = line.Split(',');
 
-            
+                    Console.WriteLine("{0}. {1}", arr);
 
-        }
+                }
 
-        sr.Close();
+                sr.Close();
+
+            }
     }
-}
+
+
+else if (resp == "2")
+{
+    Console.WriteLine("What is the name of the movie you wish to add?");
+                var newMovie = Console.ReadLine();
+                Console.WriteLine("What is the genre");
+                var newGenre = Console.ReadLine();
+            }
+
+
+
+
